044817d768ac147ec00e414722f18f3872b4c51c
ahsenelmas <149892848+ahsenelmas@users.noreply.github.com>
2026-03-29 19:31:10 +0200

    update backend changes

--- src/main/java/com/examples/studentmanagementsystem/StudentmanagementsystemApplication.java
+++ src/main/java/com/examples/studentmanagementsystem/StudentmanagementsystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing   // 🔥 THIS LINE IS CRITICAL
+@EnableJpaAuditing
 public class StudentmanagementsystemApplication {
 
     public static void main(String[] args) {
